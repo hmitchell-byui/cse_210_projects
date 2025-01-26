5f71ff3f2fa61da59f648b588e5a4fa67159073e
hmitchell-byui <hm1tch3ll99@gmail.com>
2025-01-25 19:11:37 -0600

    hw

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your number grade?");
+        string answer =  Console.ReadLine();
+        int grade = int.Parse(answer);
+        string letter_grade;
+
+        if (grade >= 90)
+        {letter_grade = "A";}
+        else if (grade >=80)
+        {letter_grade = "B";}
+        else if (grade >= 70)
+        {letter_grade = "C";}
+        else if (grade >= 60)
+        {letter_grade = "D";}
+        else
+        {letter_grade = "F";}
+        
     }
 }
