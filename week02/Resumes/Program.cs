eb9a3f0277ac60dd07f775a9c1b3c9dbe68a587d
hmitchell-byui <hm1tch3ll99@gmail.com>
2025-01-28 20:14:56 -0600

    HW

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "BYU";
+        job1._endYear = 2025;
+        job1._startYear = 2020;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Accountant";
+        job2._company = "HR_Block";
+        job2._endYear = 2022;
+        job2._startYear = 2000;
+        job2.Display();
+
     }
 }
+
